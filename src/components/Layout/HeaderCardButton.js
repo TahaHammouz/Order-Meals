63b4b953f1c369bc83e81f9583666e2e6f0fde1f
TahaHammouz <t.r.hamooz@gmail.com>
2022-08-25 17:23:28 +0300

    cart button

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,14 +1,21 @@
 import React from "react";
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCardButton.module.css";
+import { useContext } from "react";
+import CartContext from "../store/cart-context";
 const HeaderCardButton = (props) => {
+  const ctx = useContext(CartContext);
+  const Cartitemsnumber = ctx.items.reduce((current, item) => {
+    return current + item.amount;
+  }, 0);
+
   return (
     <button className={styles.button} onClick={props.isShow}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{Cartitemsnumber}</span>
     </button>
   );
 };
